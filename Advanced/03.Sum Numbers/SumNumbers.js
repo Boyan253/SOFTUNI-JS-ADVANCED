69210d6c445784b9d184f318be825030a4b86e2e
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-08-20 13:19:44 +0300

    Delete Advanced/03.Sum Numbers directory

--- Advanced/03.Sum Numbers/SumNumbers.js
+++
@@ -1,8 +0,0 @@
-function calc() {
-   let num = document.getElementById("num1").value
-   let num2 =  document.getElementById('num2').value
-
- 
-  let res =   Number(num) + Number(num2)
-  document.getElementById('sum').value = res
-}
