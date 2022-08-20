8d75e414a621904e07aed6c6c35b502be115ce1f
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-08-20 13:19:38 +0300

    Delete Advanced/02.Collect List Items directory

--- Advanced/02.Collect List Items/CollectListItems.js
+++
@@ -1,7 +0,0 @@
-function extractText() {
- let items = [...document.querySelectorAll('li')]
- 
- let finaldata = (items.map(e => e.textContent).join('\n'));
- document.getElementById('result').value = finaldata
-
-}
