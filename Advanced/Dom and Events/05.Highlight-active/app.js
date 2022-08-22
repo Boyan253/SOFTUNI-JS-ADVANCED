dd00803ae740aa49de9269894661ad55c5082650
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-08-22 11:03:14 +0300

    js

---
+++ Advanced/Dom and Events/05.Highlight-active/app.js
@@ -0,0 +1,18 @@
+function focused() {
+    Array.from(document.querySelectorAll('input'))
+   .forEach(i => {i.addEventListener('focus', onFocus);
+                  i.addEventListener('blur', onBlur)})
+
+
+
+   function onFocus(ev){
+
+ev.target.parentElement.className = 'focused'
+
+
+
+
+   }function onBlur(ev){
+    ev.target.parentElement.className = 'blured'
+   }
+}
