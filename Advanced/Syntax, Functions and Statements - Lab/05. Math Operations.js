8f7374d709b52f2e09e86872184a3fa3fbd71b4a
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-09-13 15:27:00 +0300

    Add files via upload

---
+++ Advanced/Syntax, Functions and Statements - Lab/05. Math Operations.js
@@ -0,0 +1,52 @@
+function operations(Num1,Num2,operator){
+let res 
+    switch(operator){
+
+case '+':
+   res =  Num1 + Num2
+
+break
+
+
+case "-":
+    res = Num1 - Num2
+break
+
+
+case '*':
+    res =Num1 * Num2
+
+break
+
+
+case '/': 
+res =Num1 / Num2
+
+break
+
+case '%':
+    res =  Num1 % Num2
+break
+
+
+case '**': res =Num1 ** Num2
+
+break
+
+
+
+
+
+
+
+}
+
+
+console.log(res);
+
+
+
+
+
+
+}
