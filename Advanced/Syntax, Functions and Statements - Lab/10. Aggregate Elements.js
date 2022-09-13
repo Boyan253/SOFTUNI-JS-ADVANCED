7bb1219afafef86285b56f92f035ad48113e1609
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-09-13 15:28:56 +0300

    Add files via upload

---
+++ Advanced/Syntax, Functions and Statements - Lab/10. Aggregate Elements.js
@@ -0,0 +1,16 @@
+function solve(elements) {
+    aggregate(elements, 0, (a,b) => a + b);
+    aggregate(elements, 0, (a,b) => a + 1/b);
+    aggregate(elements, '', (a,b) => a + b);
+
+    function aggregate(arr, initVal, func) {
+        let val = initVal;
+
+        for(let i = 0; i < arr.length; i++) {
+            val = func(val, arr[i]);
+        }
+        console.log(val);
+    }
+}
+
+solve([1, 2, 3]);
