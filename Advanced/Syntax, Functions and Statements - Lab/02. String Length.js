f873f0d487bbe727bb8002377b39f826c8b8d165
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-09-13 15:25:56 +0300

    Add files via upload

---
+++ Advanced/Syntax, Functions and Statements - Lab/02. String Length.js
@@ -0,0 +1,9 @@
+function string (inp,pinp,sinp){
+
+    let sum = inp.length + pinp.length + sinp.length
+    let avg = sum / 3
+    console.log(sum);
+    console.log(avg.toFixed(0));
+    
+    
+    }
