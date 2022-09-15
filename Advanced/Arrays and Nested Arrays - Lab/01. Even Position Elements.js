078dc3f1d04a660a752a3a1039a67c119be6c9ea
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-09-15 23:04:58 +0300

    Add files via upload

---
+++ Advanced/Arrays and Nested Arrays - Lab/01. Even Position Elements.js
@@ -0,0 +1,18 @@
+function evenElements(arr){
+let newarr = []
+for(let i = 0; i < arr.length; i++){
+if(i % 2 == 0){
+   newarr.push(arr[i])
+}
+
+
+
+
+
+
+}console.log(newarr.join(' '));
+
+
+
+
+}evenElements(['20', '30', '40', '50', '60'])
