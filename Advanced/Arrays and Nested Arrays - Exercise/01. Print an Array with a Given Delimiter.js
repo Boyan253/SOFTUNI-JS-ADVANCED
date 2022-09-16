75310063334171d4089041ef062677c644480310
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-09-16 23:16:44 +0300

    Add files via upload

---
+++ Advanced/Arrays and Nested Arrays - Exercise/01. Print an Array with a Given Delimiter.js
@@ -0,0 +1,26 @@
+function givenDelimeter(arr,inp){
+let newarr = []
+
+for (let el of arr){
+
+newarr.push(el)
+newarr.push(inp)
+
+
+
+
+
+}newarr.pop()
+
+
+console.log(newarr.join(''));
+
+
+
+
+}givenDelimeter(['One', 
+'Two', 
+'Three', 
+'Four', 
+'Five'], 
+'-')
