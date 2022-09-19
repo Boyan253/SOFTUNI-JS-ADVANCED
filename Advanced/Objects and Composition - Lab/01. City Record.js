385ac2ea38efdbca7bb781004e4760e926ba0b66
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-09-19 21:33:31 +0300

    Add files via upload

---
+++ Advanced/Objects and Composition - Lab/01. City Record.js
@@ -0,0 +1,19 @@
+function cityRecording(name,population,treasure){
+
+let obj = {
+
+}
+obj.name=name,
+obj.population=population,
+obj.treasury=  treasure
+
+
+return obj
+
+}cityRecording('Tortuga',
+7000,
+15000)
+
+cityRecording('Santo Domingo',
+12000,
+23500)
