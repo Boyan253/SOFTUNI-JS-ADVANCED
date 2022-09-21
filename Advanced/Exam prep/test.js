bcd34e03842a01f596b5cdf79ccc288ee629c2d8
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-09-21 20:18:23 +0300

    Add files via upload

---
+++ Advanced/Exam prep/test.js
@@ -0,0 +1,16 @@
+function partsToBuy(partsCatalog, neededParts) {
+    let totalSum = 0;
+
+    if (!Array.isArray(partsCatalog) || !Array.isArray(neededParts)) {
+      throw new Error("Invalid input");
+    }
+    neededParts.forEach((neededPart) => {
+      partsCatalog.map((obj) => {
+        if (obj.part === neededPart) {
+          totalSum += obj.price;
+        }
+      });
+    });
+
+    return totalSum;
+  } partsToBuy([300,300], [300,200])
