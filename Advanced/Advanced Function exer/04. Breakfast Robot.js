477bd0119ee00fbf8024e38df89772f422977c33
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-10-03 22:38:25 +0300

    Add files via upload

---
+++ Advanced/Advanced Function exer/04. Breakfast Robot.js
@@ -0,0 +1,61 @@
+function breakfastr(params) {
+    let apple = {
+        carbohydrate: 1,
+        flavour: 2
+    } 
+    let lemonade = {
+        carbohydrate: 10,
+        flavour: 20
+    } 
+    let burger  = {
+        carbohydrate: 5,
+        fat: 7,
+        flavour: 3
+    } 
+    let eggs = {
+        protein: 5,
+        fat: 1,
+        flavour: 1
+    }
+    let turkey = {
+        protein: 10,
+        carbohydrate: 10,
+        fat: 10,
+        flavour: 10
+    }
+
+
+let library = {
+    apple,
+    turkey,
+    eggs,
+    burger,
+    lemonade
+    
+}
+
+
+
+let flavour = 0
+let letcarbohydrate = 0
+let fat = 0
+let protein = 0
+let ar= []
+
+function restock(microel, quantity){
+if(library.has(microel)){
+
+}
+}
+
+function prepare(recipe, quantity){
+
+}
+
+function report(){
+
+}
+
+
+
+}
