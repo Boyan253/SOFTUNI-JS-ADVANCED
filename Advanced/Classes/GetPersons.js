7eb803a27a2b40476b6bcfc1a75d35a896b5f8ad
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-10-08 13:57:07 +0300

    Add files via upload

---
+++ Advanced/Classes/GetPersons.js
@@ -0,0 +1,18 @@
+function GetPersons(){
+
+    class Person{
+        constructor9(firstname,lastname,age,email){
+        this.firstname = firstname
+        this.lastname = lastname
+        this.age = age
+        this.email = email
+        }
+        toString(){return `${this.firstname} ${this.lastname} (age: ${this.age}, email: ${this.email})`
+        
+        }
+        }
+
+
+
+
+}
