4dc7dfe49e0b58a9eaed229b8cb874852bdb0283
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2022-10-19 22:54:54 +0300

    Add files via upload

---
+++ Advanced/Advanced Function Lab/07. Cars.js
@@ -0,0 +1,49 @@
+function solve(input) {
+    let data = [];
+ 
+    let action = carsManipulator();
+ 
+    for (const task of input) {
+        let [cmd, name, key, value] = task.split(' ');
+ 
+        if (key == 'inherit') {
+            cmd += key;
+            key = value;
+        }
+ 
+        action[cmd](name, key, value);
+ 
+    }
+ 
+    function carsManipulator() {
+ 
+        let result = {
+            create: (name) => {
+                data[name] = {};
+            },
+ 
+            createinherit: (name, nameOfParent) => {
+                let newObj = Object.create(data[nameOfParent]);
+                data[name] = newObj;
+            },
+ 
+            set: (name, key, value) => {
+              data[name][key] = value;
+            },
+ 
+            print: (name) => {
+              let output = [];
+              
+              for (var prop in data[name]) {
+                output.push(`${prop}:${data[name][prop]}`);
+              }
+                
+                let s = output.join(',');
+ 
+                console.log(s);
+            }
+        }
+ 
+        return result;
+    }
+}
