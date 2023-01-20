a4f14ee3c9c188009fc7d92e266581a1b8372918
Boyan Iliev <97385003+Boyan253@users.noreply.github.com>
2023-01-20 13:59:58 +0200

    Update 02. Argument Info.js

--- Advanced/Advanced Function exer/02. Argument Info.js
+++ Advanced/Advanced Function exer/02. Argument Info.js
@@ -1,3 +1,15 @@
-function arg(params) {
-    
+function info(information){
+    let result = [];
+    let count = {};
+    [...arguments].forEach(a =>{
+        let type = typeof a;
+        result.push({type, value: a});
+        if(!count[type]){
+            count[type] = 0;
+        }
+        count[type]++
+     })
+     result.forEach(r => console.log(`${r.type}: ${r.value}`));
+     let sort = Object.entries(count).sort((a, b) => b[1] - a[1])
+     .forEach(c => console.log(`${c[0]} = ${c[1]}`))
 }
